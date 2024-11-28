e7dc07e08efdb481c27665112530aeb1d6aee554
tientda1 <110662513+tientda1@users.noreply.github.com>
2024-11-28 23:08:38 +0700

    Phùng Quang Tiến

---
+++ Session 8 - Ex7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Nhap kich thuoc ma tran (n x n): ");
+    scanf("%d", &n);
+    int arr[n][n];
+    printf("Nhap cac phan tu cho ma tran:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("Nhap phan tu arr[%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("Cac phan tu trên duong cheo chinh: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i][i]);
+        sum += arr[i][i];
+    }
+    printf("\nTong cac phan tu tren duong cheo chinh la: %d\n", sum);
+    
+    return 0;
+}
+
